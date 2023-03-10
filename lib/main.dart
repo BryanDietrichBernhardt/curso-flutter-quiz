6e39feb1ead0be093642e6d88eb59e4270a109af
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-03-10 19:58:58 -0300

    Add quiz page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'quiz_page.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const Quiz());
 }
---
+++ lib/quiz_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Center(
+            child: Text("Quiz Flutter & Dart"),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Text("Pergunta 1 de 10"),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Pergunta"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  minimumSize: const Size(250, 50),
+                ),
+                child: const Text(
+                  "Resposta 1",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  minimumSize: const Size(250, 50),
+                ),
+                child: const Text(
+                  "Resposta 2",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  minimumSize: const Size(250, 50),
+                ),
+                child: const Text(
+                  "Resposta 3",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  minimumSize: const Size(250, 50),
+                ),
+                child: const Text(
+                  "Resposta 4",
+                  style: TextStyle(fontSize: 30),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
